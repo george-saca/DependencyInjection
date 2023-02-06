dc3c8eec584fa29bb2702cc3704639029a20b6d5
George S <georgesacasanu.developer@gmail.com>
2023-02-06 11:06:03 +0200

    DI through property

--- Program.cs
+++ Program.cs
@@ -5,18 +5,28 @@
         static void Main(string[] args)
         {
             IDataAccess dal = new DataAccess();
-            IBusiness business = new Business(dal);
-            var userInterface = new UserInterface(business);
+            IBusiness business = new Business();
+            business.DataAccess = dal;
+            var userInterface = new UserInterface();
+            userInterface.Business = business;
             userInterface.GetData();
         }
     }
 
     public class UserInterface
     {
-        private readonly IBusiness _business;
-        public UserInterface(IBusiness business)
+        private IBusiness _business;
+        public IBusiness Business
         {
-            _business = business;
+            set { _business = value; }
+            get 
+            {
+                if (_business == null)
+                {
+                    throw new Exception("dataAccess is not initialized");
+                }
+                return _business;
+            }
         }
         public void GetData()
         {
@@ -26,29 +36,48 @@
             Console.Write("Enter Password:");
             var password = Console.ReadLine();
 
-            IDataAccess dal = new DataAccess();
-            IBusiness business = new Business(dal);
             _business.SignUp(userName, password);
-
         }
     }
 
     public class Business : IBusiness
     {
-        private readonly IDataAccess _dataAccess;
-        public Business(IDataAccess dataAccess)
+        private IDataAccess? _dataAccess;
+        public IDataAccess DataAccess
         {
-            _dataAccess = dataAccess;
+            set { _dataAccess = value; }
+            get
+            {
+                if(_dataAccess == null)
+                {
+                    throw new Exception("dataAccess is not initialized");
+                }
+                return _dataAccess;
+            }
         }
         public void SignUp(string userName, string password)
         {
             // validation
-            _dataAccess.Store(userName, password);
+            _dataAccess?.Store(userName, password);
         }
     }
 
     public class BusinessV2 : IBusiness
     {
+        private IDataAccess? _dataAccess;
+        public IDataAccess DataAccess
+        {
+            set { _dataAccess = value; }
+            get
+            {
+                if (_dataAccess == null)
+                {
+                    throw new Exception("dataAccess is not initialized");
+                }
+                return _dataAccess;
+            }
+        }
+
         public void SignUp(string userName, string password)
         {
             
@@ -65,11 +94,13 @@
 
     public interface IBusiness
     {
+        public IDataAccess DataAccess { get; set; }
         void SignUp(string userName, string password);
     }
 
     public interface IDataAccess
     {
+        public 
         void Store(string userName, string password);
     }
 }
